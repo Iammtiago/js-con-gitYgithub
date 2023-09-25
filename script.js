af2f3b0934b0f2cd537126700687f6593aa7ca64
Tiago <118379583+Iammtiago@users.noreply.github.com>
2023-09-24 20:32:52 -0400

    un alert y console

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 console.log("Hello, world!");
 console.log("2");
+const button = document.querySelector(".boton");
+button.addEventListener("click", (e)=>{ 
+    alert("Hello baby!");
+    console.log("Hello baby");
+})
